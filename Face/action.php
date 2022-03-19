faadebf71b1a07f7446b942d617f2ad1c187d20b
HE-SHU-WEI <william900508@gmail.com>
2022-03-19 17:33:21 +0800

    20220319-2

--- Face/action.php
+++ Face/action.php
@@ -1,18 +1,21 @@
 <?php
 session_start(); //必須處於程式頂部
-// 圖片字首
-$pre          = trim($_GET['pre']);
-// 儲存的圖片路徑
-$picture_name = 'picture/' . $pre . "_" . time() . '.jpg';
+// 儲存的圖片檔名
+$picture_name = 'C:/Users/User/Desktop/abc/' . 'picture'  . "_" . time() . '.jpg';
 // 儲存圖片
 $result       = move_uploaded_file($_FILES['webcam']['tmp_name'], $picture_name);
+// move_uploaded_file(要移動的文件,文件新位置)
+// $_FILES['webcam'] = C:\xampp\tmp
+// 新位置要用一個$包起來
 
+echo $_FILES['webcam']['tmp_name'];
 
 if (!$result) {
     echo "儲存圖片失敗";
     exit();
 }
 
-$url_raw = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI'])  . $picture_name;
-$url = str_replace('\\', '/', $url_raw);
-echo "上傳成功 \n" . "圖片路徑:" . $url;
+// $url_raw = 'C:\Users\User\Pictures\Saved Pictures' ;
+// $url = str_replace('\\', '/', $url_raw);
+// echo "上傳成功 \n" . "圖片路徑:" . $url;
+echo "上傳成功 \n" . "圖片路徑:";
