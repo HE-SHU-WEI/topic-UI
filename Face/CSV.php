240dcc307a018b2d06ed255334d625494b7780fb
HE-SHU-WEI <william900508@gmail.com>
2022-05-13 11:56:55 +0800

    220513

--- Face/CSV.php
+++ Face/CSV.php
@@ -9,6 +9,17 @@ $sql ="UPDATE `$class` SET `CSV`= '$csv' WHERE `id`= '$id'";//寫入SQL
 $prepare =  $conn -> prepare($sql);
 $prepare->execute();//執行
 
+if ($prepare->execute()) {
+    echo 'Update succeeded';
+    echo '<br>3秒後跳轉到主畫面';
+    header('refresh:3; url= ../home.html');
+ 
+ } else {
+    echo 'Update failed!';
+    echo '<br>3秒後跳轉到上一頁';
+    header('refresh:3; url= face.html');
+ }
+
 
 
 ?>
